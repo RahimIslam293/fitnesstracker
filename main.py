eb5ac98f04341561799449b7186c0e9f47fbbb7d
RahimIslam293 <110697424+RahimIslam293@users.noreply.github.com>
2025-05-05 16:09:58 -0400

    Fixed API Key issue

--- main.py
+++ main.py
@@ -10,21 +10,23 @@ X_APP_ID = os.getenv("APP_ID")
 X_API_KEY = os.getenv("API_KEY")
 print(X_APP_ID)
 print(X_API_KEY)
+print(EXERCISE_URL)
 X_REMOTE_ID = "0"
 
-headers = {
+ntx_headers = {
     "x-app-id": X_APP_ID,
-    "x-api-key": X_API_KEY,
+    "x-app-key": X_API_KEY,
     "x-remote-key": X_REMOTE_ID
 }
 
 
-query = input("What workout did you do today?")
+#query = input("What workout did you do today?")
+query = "I ran three miles"
+
 
 ntx_query = {
     "query": query
 }
 
-resp = requests.post(url=EXERCISE_URL, headers=headers, json=ntx_query)
-resp.raise_for_status()
+resp = requests.post(url=EXERCISE_URL, headers=ntx_headers, json=ntx_query)
 print(resp.text)
